2e9338f08c28af204f4ad78a25c43cd257b34537
SungYong <gold9ine@gmail.com>
2014-04-11 15:29:10 +0900

    front layout css refactoring done

--- FrontWebPage/MusicHarmony/public/js/application.js
+++ FrontWebPage/MusicHarmony/public/js/application.js
@@ -1,18 +1,19 @@
 $(function(){                              
     $("button#modeChangeButton").on('click', function(){
-        var scrollArea = $('#scrollArea');
+        var containerArea = $('#container');
 
-        if(scrollArea.hasClass('normal'))
+        if(containerArea.hasClass('normal'))
         {
-          scrollArea.removeClass('normal');
-          scrollArea.addClass('detail');
+          containerArea.removeClass('normal');
+          containerArea.addClass('detail');
         }
         else
         {
-          scrollArea.removeClass('detail');
-          scrollArea.addClass('normal');
+          containerArea.removeClass('detail');
+          containerArea.addClass('normal');
         }
     });
+    
     $("button#project-delete-btn").on('click', function(){
         console.log("ddd");
         $("#content").load("/myProject");
